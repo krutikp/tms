01301a3aeb3f388ec8fbe3ca9ab052103d0e967d
krutik <krutik.k.pandya@gmail.com>
2024-03-21 20:03:26 +0530

    Task management base project created

---
+++ Task-management/src/test/java/org/tms/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
